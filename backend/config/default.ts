dbb9b075120f727fb260fe7b00bf6b48b2935ed3
Dmitry Zuev <d.zuev@dodopizza.com>
2022-04-02 10:34:28 +0300

    Update backend

--- backend/config/default.ts
+++ backend/config/default.ts
@@ -1,4 +1,5 @@
 export default {
   port: 4000,
   mongoDbConnection: "mongodb://localhost:27017/url-shortener",
+  corsOrigin: "http://localhost:3000",
 };
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,16 @@ import config from "config";
 import routes from "./routes";
 import bodyParser from "body-parser";
 import connectToDb from "./database";
+import cors from "cors";
 
 const app = express();
 const port = config.get("port");
 
+app.use(
+  cors({
+    origin: config.get("corsOrigin"),
+  })
+);
 app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Application listening to http://localhost:${port}`);
